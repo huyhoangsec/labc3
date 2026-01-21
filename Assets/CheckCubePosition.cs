8628896bdc0964a50c77415fdf160cf5408ae12c
huyhoangsec <hoangdangxa@gmail.com>
2026-01-21 15:47:51 +0700

    init

---
+++ Assets/CheckCubePosition.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+
+public class CheckCubePosition : MonoBehaviour
+{
+    void Start()
+    {
+        // World Space position
+        Vector3 worldPos = transform.position;
+
+        // Local Space position (so với Parent)
+        Vector3 localPos = transform.localPosition;
+
+        Debug.Log("World Position of Cube: " + worldPos);
+        Debug.Log("Local Position of Cube: " + localPos);
+    }
+}
